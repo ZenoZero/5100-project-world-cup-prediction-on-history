c0f60bbcf504787d265c87a5b24760e1c58f056a
aishaanwer <37453150+aishaanwer@users.noreply.github.com>
2018-04-14 22:55:32 -0400

    Update data_manipulation.R

--- data/data_manipulation.R
+++ data/data_manipulation.R
@@ -12,9 +12,6 @@ team_summary <- subset(team_summary, select = -c(city, country ))
 #subsetting the country dataset to read the names of teams only; needed for filtering the team names from history
 country_summary <- subset(country_summary, select = c(Team))
 
-#combining data from history with the match schedules for stage1 2018
-team_summary <- bind_rows(team_summary, new_summary)
-
 #filtering the final dataset to exclude data from before 1928 
 #and formatting the date column to have consistent format of dates in a new column
 team_summary <- team_summary %>% 
